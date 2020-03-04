4f4349ce2fd338b738b76c27b2977e28353fd67c
D0b100 <dariawleklik@gmail.com>
2020-03-04 11:24:18 +0100

    działa :)

--- zadanie-pobierz-dane-programisty/js/download.js
+++ zadanie-pobierz-dane-programisty/js/download.js
@@ -1,7 +1,7 @@
 function progData(){
     fetch('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php')
     .then(resp => resp.json())
-    .then(progData => {
+    .then(progData2 => {
         console.log(progData)
 
         // document.getElementById('dane-programisty').innerText = progData;
@@ -15,16 +15,35 @@ function progData(){
 
         // .innerText = 'imie: ' + progData.imie, 'nazwisko: ' + progData.nazwisko,
         //  'zawod: ' + progData.zawod, 'firma: ' + progData.firma
-        
+    
 
-        // all.innerText 
+        let name = document.createElement('p');
+        let surname = document.createElement('p');
+        let job = document.createElement('P');
+        let firm = document.createElement('P');
 
+
+        name.innerHTML = 'imię:'+ progData2.imie;
+        surname.innerHTML = 'nazwisko: '+ progData2.nazwisko;
+        job.innerHTML = 'zawod: ' + progData2.zawod;
+        firm.innerHTML = 'firm: '+ progData2.firma;
+
+
+        // document.body.appendChild(name);
+        // document.body.appendChild(surname);
+        // document.body.appendChild(job);
+        // document.body.appendChild(firm);
+
+        all.appendChild(name);
+        all.appendChild(surname);
+        all.appendChild(job);
+        all.appendChild(firm);
 
     
-        let name = document.getElementById('dane-programisty').innerText = 'imie: ' + progData.imie;
-        let surname = document.getElementById('dane-programisty').innerText = 'nazwisko:  ' + progData.nazwisko;
-        let job = document.getElementById('dane-programisty').innerText = 'zawod: ' + progData.zawod;
-        let firm = document.getElementById('dane-programisty').innerText = 'firma: ' + progData.firma;
+        // let name = document.getElementById('dane-programisty').innerText = 'imie: ' + progData.imie;
+        // let surname = document.getElementById('dane-programisty').innerText = 'nazwisko:  ' + progData.nazwisko;
+        // let job = document.getElementById('dane-programisty').innerText = 'zawod: ' + progData.zawod;
+        // let firm = document.getElementById('dane-programisty').innerText = 'firma: ' + progData.firma;
 
         
 
